f63d9f242f8f5a07bf405a6e94fdd2e325bf02e9
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-04 13:12:28 +0200

    Add main drawer

---
+++ metro_flutter_app/lib/component/main_drawer.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatelessWidget {
+  Widget buildListTile(String title, IconData icon) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 24,
+        color: Color(0xd6a80f14),
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xd6a80f14),
+        ),
+      ),
+      onTap: () {
+        // ...
+      },
+    );
+  }
+
+  Widget buildIcon(String title, String iconName, double size) {
+    String name = "images/" + iconName;
+    return ListTile(
+      leading: ImageIcon(
+        AssetImage(name),
+        color: Color(0xd6a80f14),
+        size: size,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(
+          fontFamily: 'RobotoCondensed',
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+          color: Color(0xd6a80f14),
+        ),
+      ),
+      onTap: () {
+        // Navigator.pushNamed(context, 'Home');
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 120,
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.centerLeft,
+            color: Color(0xd6a80f14),
+            // #A80F14 red
+            child: Text(
+              'User123',
+              style: TextStyle(
+                  fontWeight: FontWeight.w900,
+                  fontSize: 25,
+                  color: Color(0xFFFFFFFF)),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          buildIcon(
+            'My Tickets',
+            'ticket.png',
+            25,
+          ),
+          buildIcon(
+            'Nearset Staion',
+            'walk.png',
+            45,
+          ),
+          buildIcon(
+            'Get Route',
+            'route.png',
+            20,
+          ),
+          buildListTile(
+            'Subscription',
+            Icons.subscriptions_rounded,
+          ),
+          buildListTile(
+            'Contact Us',
+            Icons.support_agent_rounded,
+          ),
+          buildListTile(
+            'Settings',
+            Icons.settings,
+          ),
+          buildListTile(
+            'Log Out',
+            Icons.logout,
+          ),
+        ],
+      ),
+    );
+  }
+}
