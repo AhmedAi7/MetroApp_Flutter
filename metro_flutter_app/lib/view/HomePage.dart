d7ed69cb8e9ec25847889d827a7d77f901b231c5
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-05 14:32:22 +0200

    Update HomePage

--- metro_flutter_app/lib/view/HomePage.dart
+++ metro_flutter_app/lib/view/HomePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../component/main_drawer.dart';
+import '../component/CustomStyles.dart';
+import '../component/Textfeildd.dart';
 
 class homepage extends StatefulWidget {
   @override
@@ -7,17 +10,229 @@ class homepage extends StatefulWidget {
 }
 
 class _homepageState extends State<homepage> {
+  String source;
+  String destination;
+  double balance = 50.75;
+  double price = 0.0;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: MediaQuery.of(context).size.height,
-      width: double.infinity,
-      decoration: BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage("images/Background.png"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.8), BlendMode.dstIn)),
-      ),);
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/Background.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.8),
+                    BlendMode.dstIn,
+                  ),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 50),
+                    child: Stack(
+                      overflow: Overflow.visible,
+                      children: [
+                        Positioned(
+                          right: 100,
+                          child: CircleAvatar(
+                            maxRadius: 75,
+                            backgroundColor: Colors.white,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  balance.toString(),
+                                  style: TextStyle(
+                                    fontSize: 23,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                                Text(
+                                  'EGP',
+                                  style: TextStyle(
+                                    fontSize: 28,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xffa80f14),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          left: 10,
+                          child: Column(
+                            children: [
+                              SizedBox(
+                                height: 18,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  //Navigator.popAndPushNamed(context, 'Recharge');
+                                },
+                                child: Container(
+                                  width: 170,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffa80f14),
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.white,
+                                        offset: Offset(0, 1),
+                                        spreadRadius: -2,
+                                        blurRadius: 6,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Recharge",
+                                      style: TextStyle(
+                                        color: Color(0xFFFFFFFF),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              InkWell(
+                                onTap: () {
+                                  //Navigator.pushNamed(context, 'MyTickets');
+                                },
+                                child: Container(
+                                  width: 170,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xffa80f14),
+                                    borderRadius: BorderRadius.circular(20),
+                                    boxShadow: [
+                                      BoxShadow(
+                                        color: Colors.white,
+                                        offset: Offset(0, 1),
+                                        spreadRadius: -2,
+                                        blurRadius: 6,
+                                      ),
+                                    ],
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "My Tickets",
+                                      style: TextStyle(
+                                        color: Color(0xFFFFFFFF),
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 60, 5, 5),
+                    child: Column(
+                      children: [
+                        textfield(
+                          "Source",
+                          Icons.my_location,
+                          source,
+                          50,
+                          350,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(15.0),
+                          child: textfield(
+                            "Destination",
+                            Icons.location_on_outlined,
+                            destination,
+                            50,
+                            350,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  // here
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(35, 15, 5, 5),
+                    child: Row(
+                      children: [
+                        InkWell(
+                          onTap: () {
+                            //Navigator.popAndPushNamed(context, 'BuyTicket');
+                          },
+                          child: Container(
+                            width: 240,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xffa80f14),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                "Buy Ticket",
+                                style: TextStyle(
+                                  color: Color(0xFFFFFFFF),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.fromLTRB(20, 5, 5, 5),
+                          child: Container(
+                            width: 100,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              color: Color(0xFFFFFFFF),
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Center(
+                              child: Text(
+                                price.toString() + " EGP",
+                                style: TextStyle(
+                                  color: Color(0xffa80f14),
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                ),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
