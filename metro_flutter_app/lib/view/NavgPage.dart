446e01da9f2e9e282f0de8f6833b777ee3014be8
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-05 14:32:53 +0200

    Update NavgPage

--- metro_flutter_app/lib/view/NavgPage.dart
+++ metro_flutter_app/lib/view/NavgPage.dart
@@ -4,47 +4,56 @@ import 'package:metro_flutter_app/component/Appbar.dart';
 import 'package:metro_flutter_app/view/HomePage.dart';
 import 'package:metro_flutter_app/view/Settings.dart';
 import 'package:metro_flutter_app/view/SubscriptionPage.dart';
+import '../component/main_drawer.dart';
+
 class navScreen extends StatefulWidget {
   @override
   _navScreenState createState() => _navScreenState();
 }
 
 class _navScreenState extends State<navScreen> {
-  List <Widget> screens=[
+  List<Widget> screens = [
     homepage(),
     subscription(),
     settings(),
   ];
-  Map<String,IconData>icons={
-    "Home" :Icons.home,
-    "Subscription":Icons.subscriptions,
-    "Settings":Icons.settings,
+  Map<String, IconData> icons = {
+    "Home": Icons.home,
+    "Subscription": Icons.subscriptions,
+    "Settings": Icons.settings,
   };
-  int currentindex=0;
+  int currentindex = 0;
   String currentpage;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:buildAppBar(),
+      appBar: buildAppBar(),
       body: screens[currentindex],
-
-      bottomNavigationBar:BottomNavigationBar(
+      drawer: MainDrawer(),
+      bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.white,
-        type: BottomNavigationBarType.fixed
-        ,
-        items: icons.map((title, icon) => MapEntry(title, BottomNavigationBarItem(
-          icon:  Icon(icon,size: 30.0,),
-          title: Text(title),
-        ))).values.toList(),
+        type: BottomNavigationBarType.fixed,
+        items: icons
+            .map((title, icon) => MapEntry(
+                title,
+                BottomNavigationBarItem(
+                  icon: Icon(
+                    icon,
+                    size: 30.0,
+                  ),
+                  title: Text(title),
+                )))
+            .values
+            .toList(),
         currentIndex: currentindex,
         selectedFontSize: 11.0,
         selectedItemColor: Color(0xffa80f14),
         unselectedItemColor: Colors.black,
         unselectedFontSize: 11.0,
-        onTap: (index)=> setState((){
-          currentindex=index;
+        onTap: (index) => setState(() {
+          currentindex = index;
         }),
       ),
     );
   }
-  }
+}
