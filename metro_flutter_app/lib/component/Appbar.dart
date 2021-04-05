a3710fcc90f61f1708ed96c34bd82c02c73d39f3
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-05 14:30:02 +0200

    Update Appbar

--- metro_flutter_app/lib/component/Appbar.dart
+++ metro_flutter_app/lib/component/Appbar.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
 AppBar buildAppBar() {
-  return AppBar
-    (
-    backgroundColor:const Color(0xffa80f14),
+  return AppBar(
+    backgroundColor: const Color(0xffa80f14),
     title: Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Text("User221",style:
-        TextStyle(
+        Text(
+          "User221",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        Icon(
+          Icons.supervised_user_circle,
           color: Colors.white,
-        ),),
-        Icon(Icons.supervised_user_circle,color: Colors.white,)
+        )
       ],
     ),
   );
