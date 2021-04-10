ce5b30fb1ecb8627b9cbbe7d214ee78884193950
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-10 15:26:22 +0200

    Update app_root

--- metro_flutter_app/lib/src/app_root.dart
+++ metro_flutter_app/lib/src/app_root.dart
@@ -3,13 +3,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:metro_flutter_app/view/Buy%20Tickets.dart';
 import 'package:metro_flutter_app/view/ChargeWallet.dart';
-import 'package:metro_flutter_app/view/HomePage.dart';
+import 'package:metro_flutter_app/view/nearest_station.dart';
 import 'package:metro_flutter_app/view/LoginPage.dart';
 import 'package:metro_flutter_app/view/NavgPage.dart';
-import 'package:metro_flutter_app/view/Settings.dart';
 import 'package:metro_flutter_app/view/SignUp1Page.dart';
 import 'package:metro_flutter_app/view/SignUp2Page.dart';
-import 'package:metro_flutter_app/view/SubscriptionPage.dart';
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -25,9 +24,9 @@ class MyApp extends StatelessWidget {
         'Subscription': (context) => navScreen(1),
         'Settings': (context) => navScreen(2),
         'NavgPage': (context) => navScreen(0),
-        'Recharge' : (context) => chargewallet(),
-        'BuyTickets' :(context)=> buyticket(),
-        //'NearestStation': (context) =>
+        'Recharge': (context) => chargewallet(),
+        'BuyTickets': (context) => buyticket(),
+        'NearestStation': (context) => NearestStation(),
         //'GetRoute': (context) => GetRoute();
         //'ContactUs': (context) => ContactUs();
         //'LogOut': (context) => LogOut();
