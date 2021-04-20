5296a0aa19c03f2f2d056b9a3bfb05df53fd67ce
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-04-20 02:43:01 +0200

    Update app_root

--- metro_flutter_app/lib/src/app_root.dart
+++ metro_flutter_app/lib/src/app_root.dart
@@ -5,20 +5,19 @@ import 'package:metro_flutter_app/view/Buy%20Tickets.dart';
 import 'package:metro_flutter_app/view/ChargeWallet.dart';
 import 'package:metro_flutter_app/view/MyTicketss.dart';
 import 'package:metro_flutter_app/view/UseTicket.dart';
+import 'package:metro_flutter_app/view/confirm_password.dart';
+import 'package:metro_flutter_app/view/edit_email.dart';
+import 'package:metro_flutter_app/view/edit_phoneNumber.dart';
 import 'package:metro_flutter_app/view/nearest_station.dart';
 import 'package:metro_flutter_app/view/LoginPage.dart';
 import 'package:metro_flutter_app/view/NavgPage.dart';
 import 'package:metro_flutter_app/view/SignUp1Page.dart';
 import 'package:metro_flutter_app/view/SignUp2Page.dart';
-<<<<<<< Updated upstream
 import 'package:metro_flutter_app/view/contact_us.dart';
 import 'package:metro_flutter_app/view/get_route.dart';
-//import 'package:metro_flutter_app/view/reset_password.dart';
-//import 'package:metro_flutter_app/view/update_info.dart';
-=======
+import 'package:metro_flutter_app/view/reset_password.dart';
 import 'package:metro_flutter_app/view/SignUp3Page.dart';
 import 'package:metro_flutter_app/view/SignUp4Page.dart';
->>>>>>> Stashed changes
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -31,7 +30,6 @@ class MyApp extends StatelessWidget {
         'Login': (context) => LoginPage(),
         'SignUp1': (context) => SignUp1Page(),
         'SignUp2': (context) => SignUp2Page(),
-<<<<<<< Updated upstream
         'HomePage': (context) => navScreen(0),
         'Subscription': (context) => navScreen(1),
         'Settings': (context) => navScreen(2),
@@ -39,18 +37,15 @@ class MyApp extends StatelessWidget {
         'Recharge': (context) => chargewallet(),
         'BuyTickets': (context) => buyticket(),
         'NearestStation': (context) => NearestStation(),
-        //'ResetPassword': (context) => ResetPassword(),
+        'ResetPassword': (context) => ResetPassword(),
         "MyTickets": (context) => mytickets(),
         'GetRoute': (context) => GetRoute(),
         'ContactUs': (context) => ContactUs(),
-        //'LogOut': (context) => LogOut(),
         "UseTickets": (context) => useticket(),
-        //'LogOut': (context) => LogOut();
-        //'SignUp3' :(context) => SignUp3Page(),
-=======
         'SignUp3': (context) => SignUp3Page(),
-        'SignUp4' :(context) => SignUp4Page(),
->>>>>>> Stashed changes
+        'SignUp4': (context) => SignUp4Page(),
+        'EditPhoneNumber': (context) => EditPhoneNumber(),
+        'EditEmail': (context) => EditEmail(),
       },
       debugShowCheckedModeBanner: false,
       home: LoginPage(),
