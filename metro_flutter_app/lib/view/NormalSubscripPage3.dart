5b6b03017a60227c8157abe3552235ee194ceb28
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-05-12 20:51:41 +0200

    Update NormalSubscriptionPage3

--- metro_flutter_app/lib/view/NormalSubscripPage3.dart
+++ metro_flutter_app/lib/view/NormalSubscripPage3.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-class normalsub3 extends StatefulWidget {
+class NormalSubscription3 extends StatefulWidget {
   @override
-  _normalsub3State createState() => _normalsub3State();
+  _NormalSubscription3State createState() => _NormalSubscription3State();
 }
 
-class _normalsub3State extends State<normalsub3> {
+class _NormalSubscription3State extends State<NormalSubscription3> {
   @override
   Widget build(BuildContext context) {
     return Container();
