bf20240e43dc1f946387119c049efe94ad7b541c
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-05-12 20:47:26 +0200

    Update app_root

--- metro_flutter_app/lib/src/app_root.dart
+++ metro_flutter_app/lib/src/app_root.dart
@@ -5,7 +5,6 @@ import 'package:metro_flutter_app/view/Buy%20Tickets.dart';
 import 'package:metro_flutter_app/view/ChargeWallet.dart';
 import 'package:metro_flutter_app/view/MyTicketss.dart';
 import 'package:metro_flutter_app/view/UseTicket.dart';
-import 'package:metro_flutter_app/view/confirm_password.dart';
 import 'package:metro_flutter_app/view/edit_email.dart';
 import 'package:metro_flutter_app/view/edit_phoneNumber.dart';
 import 'package:metro_flutter_app/view/nearest_station.dart';
@@ -30,18 +29,18 @@ class MyApp extends StatelessWidget {
         'Login': (context) => LoginPage(),
         'SignUp1': (context) => SignUp1Page(),
         'SignUp2': (context) => SignUp2Page(),
-        'HomePage': (context) => navScreen(0),
-        'Subscription': (context) => navScreen(1),
-        'Settings': (context) => navScreen(2),
-        'NavgPage': (context) => navScreen(0),
-        'Recharge': (context) => chargewallet(),
-        'BuyTickets': (context) => buyticket(),
+        'HomePage': (context) => NavScreen(0),
+        'Subscription': (context) => NavScreen(1),
+        'Settings': (context) => NavScreen(2),
+        'NavgPage': (context) => NavScreen(0),
+        'Recharge': (context) => ChargeWallet(),
+        'BuyTickets': (context) => BuyTicket(),
         'NearestStation': (context) => NearestStation(),
         'ResetPassword': (context) => ResetPassword(),
-        "MyTickets": (context) => mytickets(),
+        "MyTickets": (context) => MyTickets(),
         'GetRoute': (context) => GetRoute(),
         'ContactUs': (context) => ContactUs(),
-        "UseTickets": (context) => useticket(),
+        "UseTickets": (context) => UseTicket(),
         'SignUp3': (context) => SignUp3Page(),
         'SignUp4': (context) => SignUp4Page(),
         'EditPhoneNumber': (context) => EditPhoneNumber(),
