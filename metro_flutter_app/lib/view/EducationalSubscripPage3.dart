8e33ecd974d6b5cc2162bca6ec14367723eb9857
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-05-12 20:49:32 +0200

    Update EducationalSubscriptionPage3

--- metro_flutter_app/lib/view/EducationalSubscripPage3.dart
+++ metro_flutter_app/lib/view/EducationalSubscripPage3.dart
@@ -5,55 +5,64 @@ import 'package:metro_flutter_app/component/Buttonn.dart';
 
 import 'NavgPage.dart';
 
-class edusub3 extends StatefulWidget {
+class EducationSubscription3 extends StatefulWidget {
   @override
-  _edusub3State createState() => _edusub3State();
+  _EducationSubscription3State createState() => _EducationSubscription3State();
 }
 
-class _edusub3State extends State<edusub3> {
+class _EducationSubscription3State extends State<EducationSubscription3> {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
-        appBar: buildAppBar(),
-    body: SingleChildScrollView(
-    physics: NeverScrollableScrollPhysics(),
-    child: Stack(
-    children :[ Container(
-    height: MediaQuery.of(context).size.height,
-    width: double.infinity,
-    decoration: BoxDecoration(
-    image: DecorationImage(
-    image: AssetImage("images/Background.png"),
-    fit: BoxFit.cover,
-    colorFilter: ColorFilter.mode(
-    Colors.black.withOpacity(0.8), BlendMode.dstIn)),
-    )),
-    Padding(
-      padding: const EdgeInsets.only(top: 140),
-      child: Container(
-        child :Column(
+    return Scaffold(
+      appBar: buildAppBar(),
+      body: SingleChildScrollView(
+        physics: NeverScrollableScrollPhysics(),
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Text("An Email sent to your college\nPlease contact your students affairs\nto Complete the subscription",style:
-                TextStyle(
-                    fontSize: 16,
-                    fontFamily: 'Segoe UI',
-                    color: Colors.white,
-                    fontStyle: FontStyle.italic,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2
-                ),),
+            Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/Background.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.8),
+                    BlendMode.dstIn,
+                  ),
+                ),
+              ),
             ),
-            SizedBox(height: 20.0,),
-            Button1("Done", navScreen(0), context, 18, 80, 170)
+            Padding(
+              padding: const EdgeInsets.only(top: 140),
+              child: Container(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        "An Email sent to your college\nPlease contact your students affairs\nto Complete the subscription",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontFamily: 'Segoe UI',
+                          color: Colors.white,
+                          fontStyle: FontStyle.italic,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    button1("Done", NavScreen(0), context, 18, 80, 170)
+                  ],
+                ),
+              ),
+            ),
           ],
         ),
-        ),
-    ),
-    ]
-    )
-    )
+      ),
     );
   }
 }
