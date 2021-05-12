fa822afa60ac04e3d58fc7672d8c0ac312565fba
Muhammad Bakr <36138708+MBakr007@users.noreply.github.com>
2021-05-12 20:46:57 +0200

    Update TicketsTypes

--- metro_flutter_app/lib/models/TicketsTypes.dart
+++ metro_flutter_app/lib/models/TicketsTypes.dart
@@ -1,19 +1,13 @@
 import 'dart:core';
-import 'package:flutter/material.dart';
 
-class product {
+class Product {
   String stations, price;
-  int id,limit;
-  product({
-    this.stations,
-    this.price,
-    this.id,
-    this.limit
-  });
+  int id, limit;
+  Product({this.stations, this.price, this.id, this.limit});
 }
 
-List<product> productss = [
-  product(stations: "9 Stations", price: "5 EGP", id: 1, limit: 9),
-  product(stations: "16 Stations", price: "7 EGP", id: 2, limit: 16),
-  product(stations: "36 Stations", price: "10 EGP", id: 3, limit: 36),
+List<Product> productss = [
+  Product(stations: "9 Stations", price: "5 EGP", id: 1, limit: 9),
+  Product(stations: "16 Stations", price: "7 EGP", id: 2, limit: 16),
+  Product(stations: "36 Stations", price: "10 EGP", id: 3, limit: 36),
 ];
