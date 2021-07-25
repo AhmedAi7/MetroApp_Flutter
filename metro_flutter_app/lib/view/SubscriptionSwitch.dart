382fdec5e1cd98f7248ab964ec0cfb4c19b3da63
Muhammad Bakr <mbakr7757@gmail.com>
2021-07-25 05:30:32 +0200

    Update SubscriptionSwitch.dart

--- metro_flutter_app/lib/view/SubscriptionSwitch.dart
+++ metro_flutter_app/lib/view/SubscriptionSwitch.dart
@@ -23,21 +23,19 @@ class _SubscriptionSwitchState extends State<SubscriptionSwitch> {
     });
 
     var jsonResponse;
-    var Url = "https://metro-user-api.azurewebsites.net/CheckSubscripe";
-    var response = await http.get(Uri.parse(Url),
-        headers: <String, String>{
-          "Content-Type": "application/json",
-          HttpHeaders.authorizationHeader: token
-        });
+    var Url = "http://localhost:8080/CheckSubscripe";
+    var response = await http.get(Uri.parse(Url), headers: <String, String>{
+      "Content-Type": "application/json",
+      HttpHeaders.authorizationHeader: token
+    });
     setState(() {
-      res=response;
+      res = response;
     });
     if (response.statusCode == 200) {
       jsonResponse = json.decode(response.body);
       print("ResponseBody : " + response.body);
-     return true;
-    }
-    else {
+      return true;
+    } else {
       setState(() {
         print(response.statusCode);
       });
@@ -45,28 +43,23 @@ class _SubscriptionSwitchState extends State<SubscriptionSwitch> {
     }
   }
 
-  List<Widget> screens = [
-    NormalSubscription1(),
-    NormalSubscription3()
-  ];
+  List<Widget> screens = [NormalSubscription1(), NormalSubscription3()];
   Widget selectedscreen;
-  int counter=0;
- http.Response res;
+  int counter = 0;
+  http.Response res;
   @override
   Widget build(BuildContext context) {
-    while(counter==0) {
-
+    while (counter == 0) {
       checkSubscripe();
 
-        if (res.statusCode==200) {
-          selectedscreen = screens[1];
-        }
-        else {
-          selectedscreen = screens[0];
-        }
+      if (res.statusCode == 200) {
+        selectedscreen = screens[1];
+      } else {
+        selectedscreen = screens[0];
+      }
 
       setState(() {
-        counter=counter+1;
+        counter = counter + 1;
       });
     }
     return selectedscreen;
