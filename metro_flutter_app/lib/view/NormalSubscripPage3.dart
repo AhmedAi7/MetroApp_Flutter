a13671cb500d3f95f05448fe3144dcf54ff02b63
Muhammad Bakr <mbakr7757@gmail.com>
2021-07-25 05:30:16 +0200

    Update NormalSubscripPage3.dart

--- metro_flutter_app/lib/view/NormalSubscripPage3.dart
+++ metro_flutter_app/lib/view/NormalSubscripPage3.dart
@@ -15,64 +15,60 @@ class NormalSubscription3 extends StatefulWidget {
 }
 
 class _NormalSubscription3State extends State<NormalSubscription3> {
-
-  Future<bool> checkSubscripe()async
-  {
-    SharedPreferences sharedPreferences=await SharedPreferences.getInstance() ;
-    String token="Bearer "+sharedPreferences.getString("token");
+  Future<bool> checkSubscripe() async {
+    SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+    String token = "Bearer " + sharedPreferences.getString("token");
     setState(() {
       print(token);
     });
 
     var jsonResponse;
-    var Url="https://metro-user-api.azurewebsites.net/CheckSubscripe";
-    var response =await http.get(Uri.parse(Url),
-        headers: <String,String>{"Content-Type":"application/json", HttpHeaders.authorizationHeader:token});
-    if(response.statusCode==200) {
+    var Url = "http://localhost:8080/CheckSubscripe";
+    var response = await http.get(Uri.parse(Url), headers: <String, String>{
+      "Content-Type": "application/json",
+      HttpHeaders.authorizationHeader: token
+    });
+    if (response.statusCode == 200) {
       jsonResponse = json.decode(response.body);
-      print("ResponseBody : "+response.body);
-      print("Response :"+jsonResponse["source"]);
+      print("ResponseBody : " + response.body);
+      print("Response :" + jsonResponse["source"]);
       setState(() {
-        source=jsonResponse["source"];
-        target=jsonResponse["target"];
-        Start_date=jsonResponse["Start_date"];
-        End_date=jsonResponse["End_date"];
-        trips_num=jsonResponse["trips_num"];
-        Start_date=Start_date.split(" ")[0];
-        End_date=End_date.split(" ")[0];
+        source = jsonResponse["source"];
+        target = jsonResponse["target"];
+        Start_date = jsonResponse["Start_date"];
+        End_date = jsonResponse["End_date"];
+        trips_num = jsonResponse["trips_num"];
+        Start_date = Start_date.split(" ")[0];
+        End_date = End_date.split(" ")[0];
       });
       setState(() {
-
-      print("NO "+jsonResponse["source"]);
+        print("NO " + jsonResponse["source"]);
       });
-    }
-    else
-    {
+    } else {
       setState(() {
         print(response.statusCode);
       });
     }
   }
-  String source="";
-  String target="";
-  String Start_date="";
-  String End_date="";
-  String trips_num="";
-  int counter=0;
+
+  String source = "";
+  String target = "";
+  String Start_date = "";
+  String End_date = "";
+  String trips_num = "";
+  int counter = 0;
   @override
   Widget build(BuildContext context) {
-    if(counter==0) {
+    if (counter == 0) {
       setState(() {
         checkSubscripe();
         counter++;
       });
     }
-    return
-      Scaffold(
-      body:Stack(
-          children: [
+    return Scaffold(
+        body: Stack(children: [
       Container(
-      height: MediaQuery.of(context).size.height,
+        height: MediaQuery.of(context).size.height,
         width: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
@@ -85,9 +81,9 @@ class _NormalSubscription3State extends State<NormalSubscription3> {
           ),
         ),
       ),
-    arrowback(context),
-    Center(
-      child: Container(
+      arrowback(context),
+      Center(
+        child: Container(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
@@ -129,16 +125,16 @@ class _NormalSubscription3State extends State<NormalSubscription3> {
                         fontFamily: 'Segoe UI',
                       ),
                     ),
-
-                    Text("Destination : $target",
-                style: TextStyle(
-                fontSize: 25,
-                color: Colors.black,
-                fontStyle: FontStyle.italic,
-                fontWeight: FontWeight.w600,
-                fontFamily: 'Segoe UI',
-                ),
-              ),
+                    Text(
+                      "Destination : $target",
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.w600,
+                        fontFamily: 'Segoe UI',
+                      ),
+                    ),
                     Text(
                       "Start Date : $Start_date",
                       style: TextStyle(
@@ -170,15 +166,13 @@ class _NormalSubscription3State extends State<NormalSubscription3> {
                 ),
               ),
               Container(
-                padding:
-                EdgeInsets.only(left: 0.0, top: 0.0, right: 0, bottom: 10.0),
+                padding: EdgeInsets.only(
+                    left: 0.0, top: 0.0, right: 0, bottom: 10.0),
                 height: 50,
                 width: 400,
                 // ignore: deprecated_member_use
                 child: RaisedButton(
-                  onPressed:(){
-
-                  },
+                  onPressed: () {},
                   //padding: EdgeInsets.all(15.0),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.only(
@@ -200,48 +194,43 @@ class _NormalSubscription3State extends State<NormalSubscription3> {
                   ),
                 ),
               ),
-
             ],
           ),
         ),
-    )
-            ,
-            SizedBox(height: 12.0)
-            , Align(
-              alignment: Alignment.bottomRight,
-              child: InkWell(
-                onTap: () async {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) =>
-                            UpdateSubscription(),
-                      ));
-                },
-                child:Padding(
-                  padding: EdgeInsets.only(bottom: 20.0),
-                  child: Container(
-                    width: 200,
-                    height: 50,
-                    decoration: CustomBoxDecoration.decorationStyle(
-                        Color(0xFFFFFFFF), 10.0),
-                    child: Center(
-                      child: Text(
-                        "Update Subscription",
-                        style: TextStyle(
-                          color: Color(0xFFFFFFFF),
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          fontStyle: FontStyle.italic,
-                        ),
-                      ),
-                    ),
+      ),
+      SizedBox(height: 12.0),
+      Align(
+        alignment: Alignment.bottomRight,
+        child: InkWell(
+          onTap: () async {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => UpdateSubscription(),
+                ));
+          },
+          child: Padding(
+            padding: EdgeInsets.only(bottom: 20.0),
+            child: Container(
+              width: 200,
+              height: 50,
+              decoration:
+                  CustomBoxDecoration.decorationStyle(Color(0xFFFFFFFF), 10.0),
+              child: Center(
+                child: Text(
+                  "Renew Subscription",
+                  style: TextStyle(
+                    color: Color(0xFFFFFFFF),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
                   ),
                 ),
               ),
             ),
-    ]
-      )
-    );
-    }
+          ),
+        ),
+      ),
+    ]));
   }
+}
