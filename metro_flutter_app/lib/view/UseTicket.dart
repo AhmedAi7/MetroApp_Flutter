cc9f000fedaa4c210b6370a26da1fad0550f5a0d
Muhammad Bakr <mbakr7757@gmail.com>
2021-07-26 05:20:32 +0200

    Update UseTicket.dart

--- metro_flutter_app/lib/view/UseTicket.dart
+++ metro_flutter_app/lib/view/UseTicket.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:metro_flutter_app/component/Appbar.dart';
 import 'package:metro_flutter_app/component/CustomStyles.dart';
 import 'package:metro_flutter_app/component/main_drawer.dart';
-import 'package:metro_flutter_app/models/Ticket.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
 class UseTicket extends StatefulWidget {
@@ -24,34 +23,29 @@ class _UseTicketState extends State<UseTicket> {
     return Scaffold(
         appBar: buildAppBar("Use Ticket"),
         drawer: MainDrawer(),
-        body: Stack(
-            children: [
-              Container(
-                  height: MediaQuery
-                      .of(context)
-                      .size
-                      .height,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("images/Background.png"),
-                      fit: BoxFit.cover,
-                      colorFilter: ColorFilter.mode(
-                        Colors.black.withOpacity(0.8),
-                        BlendMode.dstIn,
-                      ),
-                    ),
-                  )),
-              arrowback(context),
-              Center(
-                  child: QrImage(
-                    data: "Ticket-$id",
-                    version: QrVersions.auto,
-                    size: 320.0,
-                    backgroundColor: Colors.white,
+        body: Stack(children: [
+          Container(
+              height: MediaQuery.of(context).size.height,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("images/Background.png"),
+                  fit: BoxFit.cover,
+                  colorFilter: ColorFilter.mode(
+                    Colors.black.withOpacity(0.8),
+                    BlendMode.dstIn,
                   ),
-              )
-            ])
-    );
+                ),
+              )),
+          arrowback(context),
+          Center(
+            child: QrImage(
+              data: "Ticket-$id",
+              version: QrVersions.auto,
+              size: 320.0,
+              backgroundColor: Colors.white,
+            ),
+          )
+        ]));
   }
 }
